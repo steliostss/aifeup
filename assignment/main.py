03cf3ca425d7920c7a63a5747b6f96ad2f458ba9
steliostss <tsagarakis.stelios@gmail.com>
2020-03-11 16:06:14 +0000

    move user, convert everything in a class

--- assignment/main.py
+++ assignment/main.py
@@ -5,7 +5,6 @@
 # W = Wall
 # H = Hole
 # F = Finish
-# Lives
 
 # TODO 
 # track moves & pushes
@@ -15,17 +14,27 @@
 # R = Restart Level
 # Arrows
 
-Lives = 6
+class World:
 
-# read the world line by line
-# give as argument the level you want to play
-def read_world (levelid):
-    levelid = str(levelid) 
-    with open("./levels/level"+levelid+".txt") as f:
-        world = [line.split() for line in f]
-    return world
+    def __init__(self, lives):
+        super().__init__()
+        lives = 6
 
-world = read_world (1) # read world
+    # read the world line by line
+    # give as argument the level you want to play
+    def read_world (self, levelid):
+        levelid = str(levelid) 
+        with open("./levels/level"+levelid+".txt") as f:
+            world = [line.split() for line in f]
+        return world
 
-for i in world:
-    print(i)
+    def user_move (self, direction):
+        
+
+
+def main (): 
+    world = World.read_world (1) # read world
+
+
+if __name__ == "__main__":
+    main()
