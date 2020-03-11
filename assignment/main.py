bc2d0a09806c0a87bdca8bc19c9c1b18edeee7a6
steliostss <tsagarakis.stelios@gmail.com>
2020-03-11 16:37:34 +0000

    initializing moves fuvntion

--- assignment/main.py
+++ assignment/main.py
@@ -1,40 +1,47 @@
-# - = Empty
-# I = Ice
-# U = User
-# B = Box 
-# W = Wall
-# H = Hole
-# F = Finish
+'''
+    # - = Empty
+    # I = Ice
+    # U = User
+    # B = Box 
+    # W = Wall
+    # H = Hole
+    # F = Finish
 
-# TODO 
-# track moves & pushes
+    # TODO 
+    # track moves & pushes
 
-###################################
-# INPUT
-# R = Restart Level
-# Arrows
+    ###################################
+    # INPUT
+    # R = Restart Level
+    # Arrows
+'''
 
-class World:
+# read the world line by line
+# give as argument the level you want to play
+def read_world (levelid):
+    levelid = str(levelid) 
+    with open("./levels/level"+levelid+".txt") as f:
+        world = [line.split() for line in f]
+    return world
 
-    def __init__(self, lives):
-        super().__init__()
-        lives = 6
+def print_world (world):
+    for i in world: 
+        for item in i: 
+            print(item, end=' ')
+        print()
 
-    # read the world line by line
-    # give as argument the level you want to play
-    def read_world (self, levelid):
-        levelid = str(levelid) 
-        with open("./levels/level"+levelid+".txt") as f:
-            world = [line.split() for line in f]
-        return world
-
-    def user_move (self, direction):
-        
-
+# given the direction (WASD)
+# user moves in the grid (if possible)
+def user_move (direction):
+    pass    
 
 def main (): 
-    world = World.read_world (1) # read world
+    world = read_world(0) # read world
+    print_world(world)
 
+    sizeX = len(world)
+    sizeY = len(world[0])
+    print (sizeX, sizeY)
 
 if __name__ == "__main__":
     main()
