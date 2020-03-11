75f82c74779285e919561fc232c6582fea0e2863
steliostss <tsagarakis.stelios@gmail.com>
2020-03-11 14:05:39 +0000

    comments

---
+++ assignment/main.py
@@ -0,0 +1,14 @@
+import matplotlib as plt
+
+# S = Save
+# 0 = Empty Box
+# Ice = Ice Box
+# Orange = Orange Box
+# 3 = Border Wall
+# U = User
+# F = Finish
+
+###################################
+# INPUT
+# R = Restart Level
+# 
