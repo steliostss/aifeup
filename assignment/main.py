a588faff154e012864791045bcca7b6dfe085bee
steliostss <tsagarakis.stelios@gmail.com>
2020-03-11 15:07:13 +0000

    partial implementation

--- assignment/main.py
+++ assignment/main.py
@@ -1,14 +1,39 @@
 import matplotlib as plt
 
-# S = Save
-# 0 = Empty Box
-# Ice = Ice Box
-# Orange = Orange Box
-# 3 = Border Wall
+# - = Empty
+# I = Ice
 # U = User
+# B = Box 
+# W = Wall
+# H = Hole
 # F = Finish
+# Lives
+
+# TODO 
+# track moves & pushes
 
 ###################################
 # INPUT
 # R = Restart Level
-# Test
+# Arrows
+
+Lives = 6
+
+def read_world (levelid):
+    levelid = str(levelid) 
+    with open("./levels/level"+levelid+".txt") as f:
+        lines = f.read().splitlines()
+    return lines
+
+world = []
+
+lines = read_world (1)
+
+for i in lines:
+    world.append(i)
+
+    # for spot in i: 
+    #     world.append(spot)
+
+for line in world:
+    print (line)
