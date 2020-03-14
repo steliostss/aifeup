2d6b5b503bc3b6abd8ae202176973ff02c0d38c0
steliostss <tsagarakis.stelios@gmail.com>
2020-03-14 21:06:22 +0000

    minor

--- assignment/main.py
+++ assignment/main.py
@@ -11,12 +11,12 @@ import World as wd
 import sys
 
 def main ():
-    
+
     # check if program is called with the correct arguments
     if len(sys.argv) != 2:
         print('Usage: main.py <level>')
         exit(1)
-        
+
     level = sys.argv[1]
     world = wd.World(level)
     world.print_world()
