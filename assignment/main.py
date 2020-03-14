1d0355194eba6dbff9847a3b7d53938e5fc81827
steliostss <tsagarakis.stelios@gmail.com>
2020-03-14 16:05:51 +0000

    Correct usage:

--- assignment/main.py
+++ assignment/main.py
@@ -8,12 +8,12 @@
 
 import User as usr
 import World as wd
-    
+
 def main ():
     
     # check if program is called with the correct arguments
     if len(sys.argv) != 2:
-        print('Usage: main.py level')
+        print('Usage: main.py <level>')
         exit(1)
         
     level = sys.argv[1]
