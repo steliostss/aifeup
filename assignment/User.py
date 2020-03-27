b57f06dbecc2715acd88ac0751c34bdaeadf9832
steliostss <tsagarakis.stelios@gmail.com>
2020-03-27 15:21:40 +0200

    User + world + test

--- assignment/User.py
+++ assignment/User.py
@@ -1,59 +1,31 @@
 class User: 
-    def __init__(self, _position, _lives, _world):
-        self.position = self.find_user_position(_world)
-        self.lives = _lives
-
-    def find_user_position(self, _world):
-        i = 0
-        sizeX = _world.X
-        sizeY = _world.Y
-        while (i < sizeX):
-            j = 0
-            while j < sizeY:
-                if _world.map[i][j] == 'U':
-                    user_position = (i, j)
-                    return
-                j+=1
-            i+=1
-
-    def update_user_position (self, _direction):
-        x, y = self.position
-        if   (_direction == 'W'):
-            self.position = (x-1, y)
-        elif (_direction == 'S') :
-            self.position = (x+1, y)
-        elif (_direction == 'A'):
-            self.position = (x, y-1)
-        else: # _direction = 'D'
-            self.position = (x, y+1)
-
     #called when user presses W
-def move_up (world, steps=1):
-    global user_position, sizeX, sizeY
-    X, Y = user_position
-    target = world[X-1][Y]
-    if (target == "W" or target == "H"):
-        return False
-    else:
-        if (target == "F"):
-            #TODO END GAME HERE
-            pass
-        elif (target == "-"):
-            world[X-1][Y] = 'U'
-            world[X][Y] = '-'
-            user_position = (X-1, Y)            
-        else: # target == "B"
-            if (X-2 >= 0):
-                if (world[X-2][Y] == '-'):
-                    world[X-1][Y] = 'U'
-                    world[X][Y] = '-'
-                    user_position = (X-1, Y)
-                    world[X-2][Y] = 'B'
-                elif (world[X-2][Y] == 'H'):
-                    world[X-1][Y] = 'U'
-                    world[X][Y] = '-'
-                    user_position = (X-1, Y)
-                    world[X-2][Y] = '-'
+    def move_up (world, steps=1):
+        global user_position, sizeX, sizeY
+        X, Y = user_position
+        target = world[X-1][Y]
+        if (target == "W" or target == "H"):
+            return False
+        else:
+            if (target == "F"):
+                #TODO END GAME HERE
+                pass
+            elif (target == "-"):
+                world[X-1][Y] = 'U'
+                world[X][Y] = '-'
+                user_position = (X-1, Y)            
+            else: # target == "B"
+                if (X-2 >= 0):
+                    if (world[X-2][Y] == '-'):
+                        world[X-1][Y] = 'U'
+                        world[X][Y] = '-'
+                        user_position = (X-1, Y)
+                        world[X-2][Y] = 'B'
+                    elif (world[X-2][Y] == 'H'):
+                        world[X-1][Y] = 'U'
+                        world[X][Y] = '-'
+                        user_position = (X-1, Y)
+                        world[X-2][Y] = '-'
 
     #called when user presses S
     def move_down (self, world, steps=1):
@@ -103,13 +75,13 @@ def move_up (world, steps=1):
     # given the direction (WASD)
     # user moves in the grid (if possible)
     def user_move (self, world, direction):
-        if direction == "W":
+        if direction == "s":
             move_up(world)
-        elif direction == "A":
+        elif direction == "a":
             move_left(world)
-        elif direction == "S":
+        elif direction == "s":
             move_down(world)
-        elif direction == "D":
+        elif direction == "d":
             move_right(world)
         else: 
             return
--- assignment/World.py
+++ assignment/World.py
@@ -13,17 +13,21 @@ class World:
         targetX = X
         targetY = Y
         movement = self.decide_move(direction)
+        
+        # TODO: 
+        # return movement(steps)
+        # movement function will return True on success of False on Failure
 
     def decide_move(self, direction):
         # based on input movement this function returns the
         # name of the function to be called 
         movements = {
-            "up" : 'w',
-            "down" : 's',
-            "left" : 'a',
+            "up"    : 'w' ,
+            "down"  : 's' ,
+            "left"  : 'a' ,
             "right" : 'd'
         }
-        movement = movements.get(direction, '0')
+        movement = movements.get(direction, "error")
         move_func = "move"+'_'+movement
         # the move_func variable holds the name of 
         # the function that will be called to 
@@ -31,26 +35,8 @@ class World:
         # it can be called with move_func()
         return move_func
 
-    def update_positions(self, _direction, 
-                        _myPosition, _myType, 
-                        _nextPosition, _nextType):
-        # given myPositon and myType, nextPosition and nextType
-        # this function does 3 things:
-        # 1. change current position to empty space
-        # 2. change nextPosition to myType
-        # 3. change nextNextPosition to nextType 
+    def decide_action(self, ):
         pass
-
-    def update_user_position (self, _direction):
-        x, y = self.position
-        if   (_direction == 'W'):
-            self.position = (x-1, y)
-        elif (_direction == 'S') :
-            self.position = (x+1, y)
-        elif (_direction == 'A'):
-            self.position = (x, y-1)
-        else: # _direction = 'D'
-            self.position = (x, y+1)
 
     def find_user_position(self):
         # this function searches the grid and looks for
@@ -81,4 +67,5 @@ class World:
         with open("./levels/level"+levelid+".txt") as f:
             world = [line.split() for line in f]
         return world
+
 
