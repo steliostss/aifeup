e564fe61968df207f5d8917eb328db9d0740581b
steliostss <tsagarakis.stelios@gmail.com>
2020-03-31 01:11:21 +0300

    fix comments

--- assignment/Algorithms.py
+++ assignment/Algorithms.py
@@ -12,34 +12,33 @@ def DFS(world):
     best_path = sys.maxsize
 
     x,y = world.userposition
-    direction = sys.random(world.available_movements[x][y]) #Chooses an direction from the available ones
-    world.available_movements.remove(direction) #Removes this movement from the available ones because we are going to use it now
-    new_world = copy.deepcopy(world)                 #Create a copy of the our world in order to execute the movement at the new copy 
-    my_stack.append(world)                          #Add the previous instance of the world in the stack in case we want to backtrack later
+    direction = sys.random(world.available_movements[x][y]) # Chooses an direction from the available ones
+    world.available_movements.remove(direction)             # Removes this movement from the available ones because we are going to use it now
+    new_world = copy.deepcopy(world)                        # Create a copy of the our world in order to execute the movement at the new copy 
+    my_stack.append(world)                                  # Add the previous instance of the world in the stack in case we want to backtrack later
 
-    while (not my_stack): #If there are still instances of the world inside the stack continue trying to reach the end
+    while (not my_stack):                                   # If there are still instances of the world inside the stack continue trying to reach the end
 
-        result = fun.move(new_world, direction)     #Checks to see if we can make the movement we wanted at the selected direction
+        result = fun.move(new_world, direction)             # Checks to see if we can make the movement we wanted at the selected direction
 
-        if result == 0 or result == -1 :                                    #If the movement is failed 
-            if  world.available_movements[x][y].empty:                   #check if there aren't available movements left
-               world = my_stack.pop()                                    #and remove the world instance from the stack
-            else:                                                           #else
-                direction = sys.random(world.available_movements[x][y])  #try another available movement
-                world.available_movements.remove(direction)              #and remove it from the available
-        elif result == 2:                                                   #if the result is 2 it means it has reached the end
-            if len(new_world.path) < best_path:                             #then save this world as the best available path yet
-                best_world = new_world                                      #if there is a smaller path discovered
-                best_path = len(new_world.path)                             #update the best world and path
-        elif result == 1:                                                   #if the movement was successfull, it means the user has been moved
-            x,y = world.userposition                                     #so we get the new position of the user
-            direction = sys.random(world.available_movements[x][y])      #choose an available movement for the new user position
-            world.available_movements.remove(direction)                  #remove that movement for the available ones 
-            new_world = copy.deepcopy(world)                             #create copy of the world to the execute the movement
-            my_stack.append(new_world)                                      #add this new istance to the stack
+        if result == 0 or result == -1 :                                 # If the movement is failed 
+            if  world.available_movements[x][y].empty:                   # check if there aren't available movements left
+               world = my_stack.pop()                                    # and remove the world instance from the stack
+            else:                                                        # else
+                direction = sys.random(world.available_movements[x][y])  # try another available movement
+                world.available_movements.remove(direction)              # and remove it from the available
+        elif result == 2:                                                   # if the result is 2 it means it has reached the end
+            if len(new_world.path) < best_path:                             # then save this world as the best available path yet
+                best_world = new_world                                      # if there is a smaller path discovered
+                best_path = len(new_world.path)                             # update the best world and path
+        elif result == 1:                                                   # if the movement was successfull, it means the user has been moved
+            x,y = world.userposition                                     # so we get the new position of the user
+            direction = sys.random(world.available_movements[x][y])      # choose an available movement for the new user position
+            world.available_movements.remove(direction)                  # remove that movement for the available ones 
+            new_world = copy.deepcopy(world)                             # create copy of the world to the execute the movement
+            my_stack.append(new_world)                                   # add this new istance to the stack
 
-    # test
-    best_world.printWorld()
+    return best_world
 
 def BFS(world):
     
