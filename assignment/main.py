8ed8a5db941ba3ed30057b710eae0e76297652eb
billykonstas <billykonstas@outlook.com>
2020-03-31 20:52:31 +0100

    Input Menu

--- assignment/main.py
+++ assignment/main.py
@@ -14,20 +14,78 @@ import Algorithms as alg
 
 def main ():
 
+    
     # check if program is called with the correct arguments
     if len(sys.argv) != 1:
         print('Usage: python3 main.py')
         exit(1)
     # work_with_input()
-    best_solutions = [ None for line in range(1,6) ]
-    for i in range(1,6):
-        world = wd.World(i)
-        list_world = [ world ]
-        best_world = alg.BFS(list_world)
-        best_solutions[i] = best_world
-    
-    for i in best_solutions:
-        i.print_world()
+    print('Press 1 to let the program to solve the game')
+    print('Press 2 to choose level and algorithm to solve the game')
+    print('Press 3 to play the game yourself')
+    choice = input('--->>')
+
+    if choice == '1':
+        pass
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        checklist = ['a', 's', 'w', 'd', 'r']
+        movement = ''
+        levels = 6
+        res = 1
+        i = 1
+        lives = 3
+        restart = False
+        while i <= levels: 
+            world = wd.World(i, lives)
+            backup_world = copy.deepcopy(world)
+            world.print_world()
+            restart = False
+
+            while res != 2:
+                movement = input("-------------- movement? >> ")
+                while (len(movement) != 1 or movement not in checklist) and not restart:
+                    movement = input("-------------- movement? >> ")
+                if movement == 'r' or res == -1 :
+                    world.lives -= 1
+                    if world.lives > 0:
+                        lives = world.lives
+                        world = copy.deepcopy(backup_world)
+                        world.lives = lives
+                        world.print_world()
+                        continue
+                    else:
+                        i = 1
+                        print("---------DEAD---------")
+                        print()
+                        print("You have to start again from level 1. Sorry")
+                        restart = True
+                        break
+                res = fun.move(world, movement)
+                world.print_world()
+
+            if res == 2 and not restart:
+                print("\n!!!!!!!!!!!!!!!!!")
+                print("!!!! success !!!!")
+                print("!!!!!!!!!!!!!!!!!\n")
+                i += 1
+            res = 1
+
+
+        else:
+            print('Exiting..')
+
+
+        best_solutions = [ None for line in range(1,6) ]
+        for i in range(1,6):
+            world = wd.World(i)
+            list_world = [ world ]
+            best_world = alg.BFS(list_world)
+            best_solutions[i] = best_world
+        
+        for i in best_solutions:
+            i.print_world()
 
 if __name__ == "__main__":
     main()
